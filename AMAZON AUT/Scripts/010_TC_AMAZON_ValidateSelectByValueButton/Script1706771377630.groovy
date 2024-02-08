73019553a85829424d4f75a3cedb400924bc074c
Shitanshu Priyadarshi <101493579+sk3316@users.noreply.github.com>
2024-02-08 12:08:36 +0530

    Add files via upload

---
+++ AMAZON AUT/Scripts/010_TC_AMAZON_ValidateSelectByValueButton/Script1706771377630.groovy
@@ -0,0 +1,30 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.amazon.in/Hauser-Comfortable-Smudge-Writing-Refillable/dp/B0B6Q92HGV/ref=sr_1_4?crid=2W6ZM100QX20Y&keywords=pens&qid=1706803638&s=office&sprefix=pens%2Coffice-products%2C358&sr=1-4&th=1')
+
+WebUI.selectOptionByValue(findTestObject('AMAZON_ValidationOfButton_OR/select_1        2        3        4        _45ea30'), 
+    Qty, true)
+
+WebUI.delay(5)
+
+WebUI.closeBrowser()
+
